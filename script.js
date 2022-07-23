1c56362cf43230c284d639d330494121881898d7
Cleiton Reis <cleitonreis@MacBook-Pro.local>
2022-07-23 17:35:52 -0300

    Animando as seções da página.

--- script.js
+++ script.js
@@ -62,3 +62,30 @@ const initScrollSuave = () => {
     link.addEventListener('click', scrollSection);
   });  
 }
+
+initScrollSuave();
+
+const initAnimacaoScroll = () => {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+  
+    const animaScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+    
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        }
+      });
+    }
+    
+    animaScroll();
+    
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
